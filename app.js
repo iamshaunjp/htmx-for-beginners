9058e141df1dda06c3a8576a03434ec9f4fab925
iamshaunjp <shaun@thenetninja.co.uk>
2024-05-27 13:20:33 +0100

    lesson-6

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import createHomepageTemplate from './views/index.js';
 import createListTemplate from './views/list.js';
-import BOOKS_DATA from './data/data.js';
 import createBookTemplate from './views/book.js';
+import BOOKS_DATA from './data/data.js';
 
 // create app
 const app = express();
@@ -26,8 +26,16 @@ app.post('/books', (req, res) => {
 
   BOOKS_DATA.push({id, title, author});
 
-  res.send(`<li>${title}, ${author}</li>`)
+  res.redirect('/books/' + id)
 });
+
+app.get('/books/:id', (req, res) => {
+  const {id} = req.params;
+  const book = BOOKS_DATA.find(b => b.id === id);
+
+  res.send(createBookTemplate(book));
+});
+
 
 // listen to port
 app.listen(3000, () => {
